6736b23d9cadef196a10d1270b15e179e8b8c2e8
Victoria68 <mvcarrau@gmail.com>
2014-07-29 21:03:10 +0200

    Revert "canvis dia 30-03-2014"
    This reverts commit 850c817aece6c0ff36690e1151cb2874ccf5c695.
    
    Conflicts:
    	web_1307/web_falcons/_frame/search-white.png
    	web_2207/img/historia/figura3_082013.jpg
    
    Signed-off-by: Victoria68 <mvcarrau@gmail.com>

--- web_2207/funcions.php
+++ web_2207/funcions.php
@@ -4,8 +4,13 @@ function capsalera(){echo"
 <html lang=\"es\">
 <head>
 	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
-	<title>FVA</title>
-	<!--<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">-->
+	<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">
+	<title>Falcons Vallbona d'Anoia | FVA</title>
+	<meta name=\"keywords\" content=\"falcons,Vallbona d'Anoia,figures,torres humanes,colla falconera,actuacions, cecavila\">
+	<meta name=\"description\" content=\"Colla de falcons de Vallbona d'Anoia. Figures i torres humanes estàtiques i en moviment. Actuacions i cercaviles a tota Catalunya.\">
+	<meta name=\"locality\" content=\"Vallbona d'Anoia,Anoia,Barcelona,Catalunya,Europa\">
+	<meta name=\"revisit-after\" content=\"5 days\">
+	<meta name=\"robots\" content=\"index, follow \">
 	<meta name=\"author\" content=\"Brown & Grey Integral Services S.L.\">
 	<meta name=\"rating\" content=\"General\">
 	<meta name=\"generator\" content=\"Lauyan TOWeb 5.1.4.644\">
@@ -15,19 +20,31 @@ function capsalera(){echo"
 	<link rel=\"stylesheet\" href=\"css/bootstrap/css/bootstrap-responsive.css\" type='text/css'>
 	<link rel=\"stylesheet\" href=\"css/bootstrap/css/bootstrap-responsive-min.css\" type='text/css'>
 	<link rel=\"stylesheet\" href=\"css/css_falcons.css\" type='text/css'>
-	<link href=\"js/bootstrap.min.js\" />
+	<script src=\"http://code.jquery.com/jquery.js\"></script>
+    <script src=\"css/bootstrap/js/bootstrap.min.js\"></script>
+    <script src=\"css/bootstrap/js/bootstrap.js\"></script>
 	<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,500,500italic,700,700italic,900,900italic,300,300italic' rel='stylesheet' type='text/css'>
-	<style>.alert a{color:#003399}.ta-left{text-align:left}.ta-center{text-align:center}.ta-justify{text-align:justify}.ta-right{text-align:right}.float-l{float:left}.float-r{float:right}.float-lpad{float:left;padding-right:1.5em;}.float-rpad{float:right;padding-left:1.5em;}</style>
+	<script>function ucc(){var a=$.cookie(\"scwDj\");if(!a||a==\"\")b=0;else{var b=0;var c=0;do{c=a.indexOf(\")\",c);if(c>0){e=c-1;while(e>=0&&a[e]!=\"=\")e--;b+=parseInt(a.substring(e+1,c));c++}}while(c>0)}$(\"#sc_pcount\").text(b)}function onChangeSiteLang(href){var i=location.href.indexOf(\"?\");if(i>0)href+=location.href.substr(i);document.location.href=href;}</script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-53233153-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </head>";}
 
 function peu(){echo "
 	<footer>
 			<div class=\"row-fluid\">
 				<div class=\"span12\">
-					<div id=\"footerfat\" class=\"span4\"><b>Contacte</b><br>C/ Major 110<br>08785 Vallbona d'Anoia (Barcelona)<br>Tel: 628 746 305<br>fva@falconsdevallbonadanoia.com</div>
+					<div id=\"footerfat\" class=\"span4\"><b>Contacte</b><br>C/ Major 110<br>08785 Vallbona d'Anoia (Barcelona)<br>Tel: 722 55 89 67<br>fva@falconsdevallbonadanoia.com</div>
 					<div id=\"footerfat\" class=\"span4\"><strong>Hi col·labora</strong><br>Ajuntament de Vallbona d'Anoia<br>Bar Simón<br>La Botigueta<br>Brown&Grey<br></div>
-					<div id=\"footerfat\" class=\"span4\"><b>Assajos</b><br>Divendres de 20:00h a 21:30<br>Sala Jaume Calveres de l'Ajuntament de Vallbona d'Anoia</div>
+					<div id=\"footerfat\" class=\"span4\"><b>Assajos</b><br>Divendres de 20:00h a 21:30<br>a la Sala Jaume Calveres <br>de l'Ajuntament <br>de Vallbona d'Anoia</div>
 				</div>
 			</div>
 		</footer>";
@@ -35,7 +52,10 @@ function peu(){echo "
 
 ?>
 
+
+
 <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<style>.alert a{color:#003399}.ta-left{text-align:left}.ta-center{text-align:center}.ta-justify{text-align:justify}.ta-right{text-align:right}.float-l{float:left}.float-r{float:right}.float-lpad{float:left;padding-right:1.5em;}.float-rpad{float:right;padding-left:1.5em;}</style>
 
 <script src="_scripts/iealert/iealert.min.js"></script>
 <script src="_scripts/jquery/jquery.mobile.custom.min.js"></script>
